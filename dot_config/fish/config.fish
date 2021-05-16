a713fc6f61783438a23a16016ad2cb5a609b1cde
r1cep <dev.r1cep@gmail.com>
2021-05-16 17:44:07 +0900

    :wrench: Added fish configration files.

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,21 @@
+# XDG Base Directory Specification
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_CACHE_HOME "$HOME/.cache"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+
+# Editor
+set -gx EDITOR nvim
+
+# Aliaces
+alias rm="rm -i"
+alias cp="cp -i"
+alias mv="mv -i"
+
+abbr -a history " history --show-time='%Y-%m-%d %T '"
+
+# fzf
+set -gx FZF_LEGACY_KEYBINDINGS 0
+
+# Volta
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
