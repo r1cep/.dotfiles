153f062102e6acfbaba9363ee09724e84632292b
r1cep <dev.r1cep@gmail.com>
2021-11-14 15:35:37 +0900

    :wrench: Added Homebrew setting in config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -20,6 +20,12 @@ set -gx FZF_LEGACY_KEYBINDINGS 0
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
 
+# Homebrew
+if test -f /opt/homebrew/bin/brew; and status is-interactive
+    fish_add_path /opt/homebrew/bin
+    eval (/opt/homebrew/bin/brew shellenv)
+end
+
 # Starship
 set -gx STARSHIP_CONFIG "$XDG_CONFIG_HOME/starship/starship.toml"
 set -gx STARSHIP_CACHE "$XDG_CACHE_HOME/.cache/starship"
