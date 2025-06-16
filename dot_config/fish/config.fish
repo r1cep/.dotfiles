238e59f9146a50c3bb51c66efb18ef4f11de7920
r1cep <dev.r1cep@gmail.com>
2025-06-16 10:02:03 +0900

    :wrench: Added PNPM_HOME setting to config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -33,6 +33,10 @@ set -gx RUSTUP_HOME "$XDG_DATA_HOME/rustup"
 set -gx CARGO_HOME "$XDG_DATA_HOME/cargo"
 set -gx PATH "$CARGO_HOME/bin" $PATH
 
+# pnpm
+set -gx PNPM_HOME "$XDG_DATA_HOME/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+
 # Rust utilities
 #abbr -a cat "bat"
 abbr -a ls "lsd"
